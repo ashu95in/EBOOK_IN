0f49e6b789df554f34a3a4cf203e0d72a3005f50
ashu <ashutosh.95.shrivastav@gmail.com>
2025-02-27 23:31:24 -0800

    adding log function

--- main/src/contorller/userController.js
+++ main/src/contorller/userController.js
@@ -1,7 +1,30 @@
 const path = require('path');
+const fs = require('fs');
 
-exports.Home = (req,res)=>{
+exports.Home =  async (req,res)=>{
+    const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress; // Get IP
+    const time = new Date().toLocaleString(); // Get request time
+    const logDir = path.join(__dirname, '../../logs');
+    const logFile = path.join(logDir, 'requests.log');
+    const logEntry = `IP: ${ip} | Time: ${time}\n`;
+    if (!fs.existsSync(logDir)) {
+        fs.mkdirSync(logDir, { recursive: true });
+    }
+    fs.appendFile(logFile, logEntry, (err) => {
+        if (err) console.error('Error writing log:', err);
+    });
+ 
+
+
+
+
+
+
+
+
+
     res.status(200);
+    
     res.sendFile(path.join(__dirname, '../../public/pages', 'index.html'));
 
 }
