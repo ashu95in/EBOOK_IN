56e3fcbd1281f03f621103c643667460ff8fef3a
ashu <ashutosh.95.shrivastav@gmail.com>
2025-02-27 23:34:14 -0800

    removing log function

--- main/src/contorller/userController.js
+++ main/src/contorller/userController.js
@@ -2,17 +2,17 @@ const path = require('path');
 const fs = require('fs');
 
 exports.Home =  async (req,res)=>{
-    const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress; // Get IP
-    const time = new Date().toLocaleString(); // Get request time
-    const logDir = path.join(__dirname, '../../logs');
-    const logFile = path.join(logDir, 'requests.log');
-    const logEntry = `IP: ${ip} | Time: ${time}\n`;
-    if (!fs.existsSync(logDir)) {
-        fs.mkdirSync(logDir, { recursive: true });
-    }
-    fs.appendFile(logFile, logEntry, (err) => {
-        if (err) console.error('Error writing log:', err);
-    });
+    // const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress; // Get IP
+    // const time = new Date().toLocaleString(); // Get request time
+    // const logDir = path.join(__dirname, '../../logs');
+    // const logFile = path.join(logDir, 'requests.log');
+    // const logEntry = `IP: ${ip} | Time: ${time}\n`;
+    // if (!fs.existsSync(logDir)) {
+    //     fs.mkdirSync(logDir, { recursive: true });
+    // }
+    // fs.appendFile(logFile, logEntry, (err) => {
+    //     if (err) console.error('Error writing log:', err);
+    // });
  
 
 
